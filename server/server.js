a1cc15e83e00956f9a250a9c9afde4f8a1e1d1e1
DontEatMyBlue <simplehyun00@gmail.com>
2023-11-21 12:09:08 +0900

    test

--- server/server.js
+++ server/server.js
@@ -100,6 +100,26 @@ app.post('/inputpost',function(req,res){
     res.redirect("/post");
     
 })
+app.post('/inputpost1',function(req,res){
+    let sent = req.body.contents;
+    let regex = /\[(.*?)\]/g;
+    let ex;
+    while((ex=regex.exec(sent))!==null){
+        result.push(ex[1]);
+    }
+    
+    let sql = 'insert into post (Contents,Me,You,SkillName1,SkillName2,SkillName3,SkillName4,SkillName5) values (?,?,?,?,?,?,?,?)';
+    let param = [req.body.contents,postme,postyou,result[0],result[1],result[2],result[3],result[4]];
+    connection.query(sql,param,function(err){
+        if(err){
+            console.log(err);
+        }
+    })
+
+    res.redirect("/post");
+    
+})
+
 
 // app.post('/write1',function(req,res){
 //     let sent = req.body.contents;
@@ -131,19 +151,32 @@ app.get('/write',function(req,res,next){
 //     result = [];
 
 // })
+let postme = "";
+let postyou = "";
+app.post('/postsearch',function(req,res){
+    postme = req.body.me;
+    res.redirect('/post' );
+  })
+  app.post('/postsearch1',function(req,res){
+    postyou = req.body.you;
+    res.redirect('/post' );
+  })
+
 
 app.get('/post',function(req,res){
-    let sql = "SELECT P.Contents,P.SkillName1, S1.SkillEx AS SkillEx1, S1.SkillImg AS SkillImg1, P.SkillName2, S2.SkillEx AS SkillEx2, S2.SkillImg AS SkillImg2, P.SkillName3, S3.SkillEx AS SkillEx3, S3.SkillImg AS SkillImg3 FROM Post AS P LEFT JOIN Skill AS S1 ON P.SkillName1 = S1.SkillName LEFT JOIN Skill AS S2 ON P.SkillName2 = S2.SkillName LEFT JOIN Skill AS S3 ON P.SkillName3 = S3.SkillName WHERE ME = '인파이터' AND YOU='리퍼' ; "
-    connection.query(sql,function(err,rows){
+
+    let sql = "SELECT P.Contents,P.SkillName1, S1.SkillEx AS SkillEx1, S1.SkillImg AS SkillImg1, P.SkillName2, S2.SkillEx AS SkillEx2, S2.SkillImg AS SkillImg2, P.SkillName3, S3.SkillEx AS SkillEx3, S3.SkillImg AS SkillImg3 FROM Post AS P LEFT JOIN Skill AS S1 ON P.SkillName1 = S1.SkillName LEFT JOIN Skill AS S2 ON P.SkillName2 = S2.SkillName LEFT JOIN Skill AS S3 ON P.SkillName3 = S3.SkillName WHERE ME =? AND YOU=? ; "
+    let param = [postme,postyou];
+    connection.query(sql,param,function(err,rows){
         if(err) console.error;       
-        console.log(rows);
         res.render("post",{rows,rows})
     })
 })
 
   let jobname ="";
   app.post('/search',function(req,res){
-    jobname = req.body.JobName;
+    jobname = req.body.job;
+    
     var string = encodeURIComponent(jobname);
     res.redirect('/skilldb/' );
   })
